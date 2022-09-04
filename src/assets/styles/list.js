0d8a8907c0e5e07d9df56b78007917e312795d29
Thushara Samaraweera <thusharadasun204@gmail.com>
2022-09-04 06:29:07 +0530

    add type selector

---
+++ src/assets/styles/list.js
@@ -0,0 +1,12 @@
+import { alpha, makeStyles } from "@material-ui/core";
+
+export default makeStyles((theme) => ({
+  container: {
+    padding: "25px",
+  },
+  formControl: {
+    margin: theme.spacing(1),
+    minWidth: 120,
+    marginBottom: "30px",
+  },
+}));
--- src/components/List.jsx
+++ src/components/List.jsx
@@ -1,8 +1,25 @@
-import React from 'react'
+import { FormControl, InputLabel, Select, Typography } from '@material-ui/core'
+import React, { useState } from 'react'
+import useStyles from '../assets/styles/list'
 
 const List = () => {
+  const classes = useStyles()
+  const [type, setType] = useState('restaurants')
+
   return (
-    <div>List</div>
+    <div className={classes.container}>
+      <Typography variant='h4'>
+        Restaurants, Hotels & Attractions around you
+      </Typography>
+      <FormControl className={classes.formControl}>
+        <InputLabel>Type</InputLabel>
+        <Select value={type} onChange={(e) => setType(e.target.value)}>
+          <option value='restaurants'>Restaurants</option>
+          <option value='hotels'>Hotels</option>
+          <option value='attractions'>Attractions</option>
+        </Select>
+      </FormControl>
+    </div>
   )
 }
 
